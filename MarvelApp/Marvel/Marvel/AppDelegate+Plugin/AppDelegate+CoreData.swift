384f220a90df2ff57ed64c2556af538678c94ffe
Maksim Kazachkov <maksimkn.94@gmail.com>
2020-02-08 20:11:09 +0300

    Minor folder management

---
+++ MarvelApp/Marvel/Marvel/AppDelegate+Plugin/AppDelegate+CoreData.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate+CoreData.swift
+//  Marvel
+//
+//  Created by Maksim Kazachkov on 08.02.2020.
+//  Copyright © 2020 Maksim Kazachkov. All rights reserved.
+//
+
+import Foundation
